5b900951d025d3d3065a983fc1f0e6ea74b4173e
Lucía González <86147223+luciagonzalezm@users.noreply.github.com>
2022-04-18 23:29:15 +0200

    Add files via upload

---
+++ Cliente Web/cliente-angular/proyecto-empresa/src/app/componentes/inicio/inicio.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Inicio');
+  }
+
+}
